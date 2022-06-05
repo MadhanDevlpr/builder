539992659329b260b5c62ba8105449bbcd0e5ceb
MadhanDevlpr <pmaadithya09@gmail.com>
2022-06-05 15:23:17 +0530

    development commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+projects = '/home/codemap/Documents/Projects/Python'
+
+
+
+def create_project():
+    folder = sys.argv[1]# Get input of the folder's name
+
+    os.chdir(projects) # Going into my projects directory
+    os.system(f'mkdir {folder}') # Making the folder
+    os.chdir(f'{projects}/{folder}')
+
+    os.system('touch README.md') # creating a readme file
+
+    os.system('subl .')
+
+
+
+create_project()
